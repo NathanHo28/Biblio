fabe55d17398e66faa4a6cea8a9ed0cdcf88deee
Jimi Vyas <jimipvyas@gmail.com>
2014-12-09 15:05:08 -0500

    create page controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'pages/create'
+
+  get 'pages/destroy'
+
+  get 'pages/new'
+
+  get 'pages/show'
+
+  get 'pages/edit'
+
+  get 'pages/update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
