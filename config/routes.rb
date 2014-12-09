935c5619ff8441717913fab7532f8be00d959c0e
Manny Berkal-Sarbit <mannyberkalsarbit@gmail.com>
2014-12-09 15:36:11 -0500

    Add pins controller with create, destroy and show aswell as private params and before filters

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pins/new'
+
+  get 'pins/create'
+
+  get 'pins/destroy'
+
+  get 'pins/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
