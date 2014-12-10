1b5d7c1adab399866f2bc957943f4207aba293e6
merrick read <merrick.read@gmail.com>
2014-12-10 15:16:04 -0500

    made a test user in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create!(
+	:first_name => "Test",
+	:last_name => "Man",
+	:username => "tester",
+	:email => "test@ing.com",
+	:password => "123",
+	:password_confirmation => "123"
+
+	)
