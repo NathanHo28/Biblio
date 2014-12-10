f13c8e3b757cef03a2d383d2eac989ca4a6baaae
Jimi Vyas <jimipvyas@gmail.com>
2014-12-10 14:31:54 -0500

    add upload mount

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,4 +3,5 @@ class Story < ActiveRecord::Base
 	has_many :pins
 	has_many :pinned_bys, through: :pins, source: :user
 	belongs_to :owner, class_name: 'User'
+	mount_uploader :page_photo, PagePhotoUploader #this is the uploader i think
 end
