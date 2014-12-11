c7beb175f948f7adf100e8d2decb6488dfba3991
NATHAN <nathan.ho28@gmail.com>
2014-12-11 11:12:01 -0500

    Fix param name to :photo_path from :image

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,4 +3,5 @@ class Page < ActiveRecord::Base
 	belongs_to :story
 	mount_uploader :page_photo, PagePhotoUploader #this is the uploader i think
 
+	# accepts_nested_attributes_for :image, :reject_if => :all_blank, :allow_destroy => true
 end
