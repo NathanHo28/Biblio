e56fb8f7c0ceea04c397ad87730ddba372bbc11a
merrick read <merrick.read@gmail.com>
2014-12-11 16:52:39 -0500

    associations for user model and relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   has_many :own_stories, class_name: 'Story', foreign_key: 'owner_id'
   has_many :pins
   has_many :pinned_stories, through: :pins
+  has_many :active_relationships, class_name: "Relationship",
+  								  foreign_key: "follower_id", #user following another user identified with different foreign key
+  								  dependent: :destroy #destroying a user should also destroy a relationship
 
   validates :password, length: {minimum: 3}
   validates :password, confirmation: true
