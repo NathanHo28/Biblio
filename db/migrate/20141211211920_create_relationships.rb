1c2bc990b97ceb28bbb93153e4862b6c27d3c97a
merrick read <merrick.read@gmail.com>
2014-12-11 20:53:34 -0500

    fixed migration for relationship foreign keys. added faker gem for users and relationships (not yet complete)

--- db/migrate/20141211211920_create_relationships.rb
+++ db/migrate/20141211211920_create_relationships.rb
@@ -2,7 +2,7 @@ class CreateRelationships < ActiveRecord::Migration
   def change
     create_table :relationships do |t|
       t.integer :follower_id
-      t.integer :followed
+      t.integer :followed_id
 
       t.timestamps null: false
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,34 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.create!(
-	:first_name => "Test",
-	:last_name => "Man",
-	:username => "tester",
-	:email => "test@ing.com",
-	:password => "123",
-	:password_confirmation => "123"
-
+User.create!(first_name: "Test",
+	last_name: "Man",
+	username: "tester",
+	email: "example@ftest.com",
+	password: "123",
+	password_confirmation: "123"
 	)
+
+99.times do |n|
+	first_name = Faker::Name.first_name
+	last_name = Faker::Name.last_name
+	username = Faker::Name.username
+	email = "example-#{n+1}@ftest.com"
+	password = "password"
+	User.create!(first_name: first_name,
+				 last_name: last_name,
+				 username: username,
+				 email: email,
+				 password: password,
+				 password_confirmation: password
+				 )
+end
+
+#following relationships
+
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
