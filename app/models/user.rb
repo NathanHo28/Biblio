66eb6ad7174f6776968dc205c0aee1010b382128
NATHAN <nathan.ho28@gmail.com>
2014-12-14 12:38:47 -0500

    Tidy up associations for better readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,19 @@ class User < ActiveRecord::Base
   has_many :active_relationships, class_name: "Relationship",
   								  foreign_key: "follower_id", #user following another user identified with different foreign key
   								  dependent: :destroy #destroying a user should also destroy a relationship
+
   has_many :passive_relationships, class_name: "Relationship", #passive relation because possibility of not following back
   								   foreign_key: "followed_id",
   								   dependent: :destroy
+
   has_many :following, through: :active_relationships, source: :followed #explicitly tells rails that the source of the following arrays is the set of followed ids
   has_many :followers, through: :passive_relationships, source: :follower #source not needed but helps to illustrate structure
+
   validates :password, length: {minimum: 3}
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
   validates :email, uniqueness: true
+
   mount_uploader :photo, PhotoUploader
 
   # def feed
