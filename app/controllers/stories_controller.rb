af771a0c12dbb9f079558041d70cf38fa09f5c93
NATHAN <nathan.ho28@gmail.com>
2014-12-14 21:55:28 -0500

    Add action to handle two different search fields

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,19 +2,43 @@ class StoriesController < ApplicationController
   # before_filter :require_login, except: [:index, :show]
 
   def index
-    @stories = if params[:search]
-      results = (Story.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%") +
-                 Story.tagged_with(params[:search], :any => true)).uniq
-      Kaminari.paginate_array(results)
-      # page cannot ordinarily be called on arrays so need to call special paginate method
+    @stories = if params[:story_fields]
+      results = search
     else
       Story.order('stories.created_at DESC')
     end.page(params[:page])
 
-    respond_to do |format|
-      format.html
-      format.js
+    # @stories = if params[:search]
+    #   results = (Story.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%") +
+    #              Story.tagged_with(params[:search], :any => true)).uniq
+    #   Kaminari.paginate_array(results)
+    #   # page cannot ordinarily be called on arrays so need to call special paginate method
+    # else
+    #   Story.order('stories.created_at DESC')
+    # end.page(params[:page])
+
+    # respond_to do |format|
+    #   format.html
+    #   format.js
+    # end
+  end
+
+  def search
+    search_params
+    sanity_check = search_params.delete_if {|category, value| value.blank?}
+    inquiry = Story.all 
+    sanity_check.each do |key, column|
+
+      if key == "title"
+        inquiry = inquiry.where(["#{key} iLIKE ?", "%#{column}%"])
+      end
+
+      if key == "tag_list"
+        inquiry = inquiry.tagged_with("#{column}", :any => true)
+      end
     end
+    
+    inquiry
   end
 
   def new
@@ -76,5 +100,9 @@ class StoriesController < ApplicationController
   private
   def story_params
     params.require(:story).permit(:title, :tag_list, pages_attributes: [:page_photo, :caption, :page_number, :story_id])
+  end
+
+  def search_params
+    params.require(:story_fields).permit(:title, :tag_list)
   end
 end
