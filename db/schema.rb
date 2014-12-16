433fb019fc47345d97b30b069f480cf20fd82b14
Jimi Vyas <jimipvyas@gmail.com>
2014-12-16 17:00:10 -0500

    state and country migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215171935) do
+ActiveRecord::Schema.define(version: 20141216215901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20141215171935) do
     t.decimal  "latitude"
     t.decimal  "longitude"
     t.string   "city"
+    t.string   "state"
+    t.string   "country"
   end
 
   create_table "taggings", force: true do |t|
