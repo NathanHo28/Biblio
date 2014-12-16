7c736ab33ce56146745fa51d65c2a0ced2f3a090
Jimi Vyas <jimipvyas@gmail.com>
2014-12-16 16:57:54 -0500

    adding geocoder support to model

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,11 +4,21 @@ class Story < ActiveRecord::Base
 	has_many :pinned_bys, through: :pins, source: :user
 	belongs_to :owner, class_name: 'User'
 
+	geocoded_by :address   # can also be an IP address
+	after_validation :geocode          # auto-fetch coordinates
+
+	#not sure we need these
+	reverse_geocoded_by :latitude, :longitude
+	after_validation :reverse_geocode
+
 	accepts_nested_attributes_for :pages, :reject_if => :all_blank, :allow_destroy => true
 	acts_as_taggable_on :tags
 	acts_as_votable
 
 	# validate :page_limit
+
+	def address
+
 
 	def score
 		self.get_upvotes.size
