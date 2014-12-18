c117a34e433c1694da678da1246f1fc07c413aba
Manny Berkal-Sarbit <mannyberkalsarbit@gmail.com>
2014-12-18 14:12:51 -0500

    fix jquery loading in app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,18 +11,14 @@
 // about supported directives.
 //
 //= require jquery
-//= require fancybox 
+//= require fancybox
 //= require select2
 //= require jquery_ujs
-<<<<<<< HEAD
 //= require foundation
-//= require turbolinks
-=======
->>>>>>> 930aec84ac05fd64cadfd1efe79d621a5fb69fe7
 //= require cocoon
 //= require_tree .
 
 
 
 
-$(function(){ $(document).foundation(); });
+$(function() { $(document).foundation(); });
