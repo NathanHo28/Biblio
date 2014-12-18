3d5f31b4642abb4c424205c667c642f980c9ea96
Jimi Vyas <jimipvyas@gmail.com>
2014-12-18 15:00:07 -0500

    fixed spelling that broke migration that i think was overwritten by a bad push/merge yesterday

--- db/migrate/20141215171905_add_longitude_to_stories.rb
+++ db/migrate/20141215171905_add_longitude_to_stories.rb
@@ -1,4 +1,4 @@
-class AddLongituteToStories < ActiveRecord::Migration
+class AddLongitudeToStories < ActiveRecord::Migration
   def change
     add_column :stories, :longitude, :decimal, :default => 0, :null => false
   end
