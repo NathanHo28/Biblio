8950b619d372861a4cea504dee21101fa24a1260
merrick read <merrick.read@gmail.com>
2014-12-18 18:51:42 -0500

    Created new Group model to act as join table between User model and Story model. It's purpose is to facilitate multiple users with different authorities to contribute and post one story

--- db/migrate/20141218234924_create_groups.rb
+++ db/migrate/20141218234924_create_groups.rb
@@ -3,7 +3,7 @@ class CreateGroups < ActiveRecord::Migration
     create_table :groups do |t|
       t.integer :story_id
       t.integer :user_id
-      t.boolean :owner
+      t.integer :state
 
       t.timestamps
     end
