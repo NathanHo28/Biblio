62262720c4440c88af16f5bfead881402509b6e3
merrick read <merrick.read@gmail.com>
2014-12-18 18:46:46 -0500

    Rolledback migration and deleted group class to remake it and define user authority with state integers instead of boonean true/false

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218222429) do
+ActiveRecord::Schema.define(version: 20141217020638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "groups", force: true do |t|
-    t.integer  "story_id"
-    t.integer  "user_id"
-    t.boolean  "owner"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "pages", force: true do |t|
     t.string   "page_photo"
