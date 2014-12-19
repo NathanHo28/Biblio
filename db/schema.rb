6a4d76b6313c6b22f9a04178ea4fb97852f1ceea
merrick read <merrick.read@gmail.com>
2014-12-19 00:00:43 -0500

    added class names to all submit buttons

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219001134) do
+ActiveRecord::Schema.define(version: 20141219013708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: true do |t|
+    t.integer  "story_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pages", force: true do |t|
     t.string   "page_photo"
@@ -107,6 +114,7 @@ ActiveRecord::Schema.define(version: 20141219001134) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
+    t.integer  "status",                          default: 0
     t.decimal  "latitude"
     t.decimal  "longitude"
     t.string   "country"
