5b9cd01a067c567c7b1477575ab086b6cc21f7ce
NATHAN <nathan.ho28@gmail.com>
2015-01-15 22:54:57 -0500

    Add medium photo version for profile picture

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -34,6 +34,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb do
     # process :resize_to_fit => [50, 50]
+    process :resize_and_crop => [50]
+  end
+
+  version :medium do
+    # process :resize_to_fit => [50, 50]
     process :resize_and_crop => [300]
   end
 
