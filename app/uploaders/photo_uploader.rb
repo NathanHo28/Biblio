86a48d2abe2f2e4ef3aa397e852b10452a14cfbf
Jimi Vyas <jimipvyas@gmail.com>
2015-01-19 21:08:34 -0500

    fixed photos not showing up and usernames not showing up if not logged in

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -20,7 +20,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
     "avatar.jpg"
-  
+
     # "/images/" + [version_name, "default.png"].compact.join('_')
   end
 
