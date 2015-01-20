018d3b34aefe4ecab76d396e7126bd19e47a5e37
merrick read <merrick.read@gmail.com>
2015-01-19 22:32:16 -0500

    Style buttons on story creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :pages, through: :own_stories
   has_many :own_stories, class_name: 'Story', foreign_key: 'owner_id'
   has_many :other_stories, through: :following, source: :own_stories # use this relation to create the feed structure
-
+ 
   has_many :contributions
   has_many :contributed_stories, class_name: "Story", through: :story_contributors
 
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
 
   has_many :pins
   has_many :pinned_stories, through: :pins
-  
+
   def followers
     Relationship.where(followed: self).includes(:follower).map(&:follower)
   end
